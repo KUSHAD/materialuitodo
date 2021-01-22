c0a9853d9264414a256240672071c03e469767e5
KUSHAD <kushadck@gmail.com>
2021-01-22 11:09:37 +0530

    Modified The App Is Running Fine Console.log()

--- src/index.js
+++ src/index.js
@@ -4,5 +4,20 @@ import App from './App/App';
 const appElement = document.getElementById('app');
 
 render(<App />, appElement, () => {
-	console.log('App is running fine !!');
+	// eslint-disable-next-line no-unused-expressions
+	window.console &&
+		(setTimeout(function () {
+			return console.log(
+				'%c%s',
+				'color: red; background: yellow; font-size: 24px;',
+				'WARNING!'
+			);
+		}),
+		setTimeout(function () {
+			return console.log(
+				'%c%s',
+				'font-size: 18px;',
+				"Using this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.\nDo not enter or paste code that you don't understand."
+			);
+		}));
 });
