a32e0572bc52e0f42a66785b4a2f0d572f560015
KUSHAD <kushadck@gmail.com>
2021-01-27 11:32:37 +0530

    created ui for feedack page

---
+++ src/App/Screens/Main/FeedBack/FeedBack.js
@@ -0,0 +1,102 @@
+import { Grid, Paper, Typography } from '@material-ui/core';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import bugReportIcon from '../../../../assets/bugReportsIcon.png';
+import commentsIcon from '../../../../assets/commentsIcon.png';
+import featureRequestIcon from '../../../../assets/featureRequestIcon.png';
+import writeCommentsIcon from '../../../../assets/writeCommentIcon.png';
+function FeedBack() {
+	return (
+		<Grid
+			container
+			style={{
+				display: 'flex',
+				height: '90vh',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<Grid
+				item
+				style={{
+					margin: 50,
+				}}
+			>
+				<Paper>
+					<img
+						style={{
+							height: 200,
+							width: 200,
+						}}
+						src={commentsIcon}
+						alt="Comments Icon"
+					/>
+					<Typography variant="h6">See Comments</Typography>
+				</Paper>
+			</Grid>
+			<Grid
+				item
+				style={{
+					margin: 50,
+				}}
+			>
+				<Paper>
+					<img
+						style={{
+							height: 200,
+							width: 200,
+						}}
+						src={featureRequestIcon}
+						alt="Feature Request Icon"
+					/>
+					<Typography variant="h6">See Feature Requests</Typography>
+				</Paper>
+			</Grid>
+			<Grid
+				item
+				style={{
+					margin: 50,
+				}}
+			>
+				<Paper>
+					<img
+						style={{
+							height: 200,
+							width: 200,
+						}}
+						src={bugReportIcon}
+						alt="Bug Report Icon"
+					/>
+					<Typography variant="h6">See Bug Reports</Typography>
+				</Paper>
+			</Grid>
+			<Grid
+				item
+				style={{
+					margin: 50,
+				}}
+			>
+				<Link
+					to="/feedback/new"
+					style={{
+						textDecorationLine: 'none',
+					}}
+				>
+					<Paper>
+						<img
+							style={{
+								height: 200,
+								width: 200,
+							}}
+							src={writeCommentsIcon}
+							alt="Write Comment Icon"
+						/>
+						<Typography variant="h6">Write a Feedback</Typography>
+					</Paper>
+				</Link>
+			</Grid>
+		</Grid>
+	);
+}
+
+export default FeedBack;
--- src/App/Screens/Main/index.js
+++ src/App/Screens/Main/index.js
@@ -1,2 +1,3 @@
+export { default as FeedBackScreen } from './FeedBack/FeedBack';
 export { default as ProfileScreen } from './Profile/Profile';
 export { default as TodoScreen } from './Todo/Todo';
