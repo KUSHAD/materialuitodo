3f51b7ef41d4e1512432b3f04963b17bbf6c6a51
KUSHAD <kushadck@gmail.com>
2021-02-01 11:47:01 +0530

    Wrapped The Bugs,Feedback and Feature screens in error boundaries

--- src/App/Screens/Main/FeedBack/FeatureRequest/FeatureRequest.js
+++ src/App/Screens/Main/FeedBack/FeatureRequest/FeatureRequest.js
@@ -9,6 +9,7 @@ import {
 } from '@material-ui/core';
 import React, { Component } from 'react';
 import { firebaseRealTimeDatabase } from '../../../../../imports';
+import { AppErrorBoundary } from '../../../../Error';
 class FeatureRequests extends Component {
 	constructor(props) {
 		super(props);
@@ -43,38 +44,40 @@ class FeatureRequests extends Component {
 				<Typography variant="h3">Feature Requests</Typography>
 				<Grid container>
 					<List>
-						{this.state.userComments.map((user) => (
-							<Grid item xs={12} key={user.createdAt}>
-								<ListItem>
-									<Paper>
-										<Grid>
-											<ListItemAvatar>
-												<Avatar
-													style={{
-														width: 50,
-														height: 50
-													}}
-													src={user.imageURL}
-													alt={user.userName}
-												/>
-											</ListItemAvatar>
-											<Typography variant="body1">
-												Name :- {user.name}
-											</Typography>
-											<Typography variant="body1">
-												Email :- {user.email}
-											</Typography>
-											<Typography variant="body1">
-												Username :- {user.userName}
-											</Typography>
-											<Typography variant="h6">
-												Comment :- {user.feedback}
-											</Typography>
-										</Grid>
-									</Paper>
-								</ListItem>
-							</Grid>
-						))}
+						<AppErrorBoundary>
+							{this.state.userComments.map((user) => (
+								<Grid item xs={12} key={user.createdAt}>
+									<ListItem>
+										<Paper>
+											<Grid>
+												<ListItemAvatar>
+													<Avatar
+														style={{
+															width: 50,
+															height: 50
+														}}
+														src={user.imageURL}
+														alt={user.userName}
+													/>
+												</ListItemAvatar>
+												<Typography variant="body1">
+													Name :- {user.name}
+												</Typography>
+												<Typography variant="body1">
+													Email :- {user.email}
+												</Typography>
+												<Typography variant="body1">
+													Username :- {user.userName}
+												</Typography>
+												<Typography variant="h6">
+													Comment :- {user.feedback}
+												</Typography>
+											</Grid>
+										</Paper>
+									</ListItem>
+								</Grid>
+							))}
+						</AppErrorBoundary>
 					</List>
 				</Grid>
 			</Grid>
